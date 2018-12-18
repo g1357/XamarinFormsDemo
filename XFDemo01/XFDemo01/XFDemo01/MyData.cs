89910057e04b4e31cd3860912e65ae9d0a0a01d5
Grigory Dukarevich <Dukarevich_G@epluse.onmicrosoft.com>
2018-12-18 21:43:55 +0300

    Adds MVVM

--- XFDemo01/XFDemo01/XFDemo01/MyData.cs
+++ XFDemo01/XFDemo01/XFDemo01/MyData.cs
@@ -1,23 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using XFDemo01.Helpers;
+
 namespace XFDemo01
 {
-    public class MyData
+    public class MyData : Observable
     {
         public string MyName { get; set; }
 
         private string myLastname = "Моя Фамилия";
         public string MyLastname {
-            get
-            {
-                return myLastname;
-            }
-            set
-            {
-                myLastname = value;
-                
-            }
+            get { return myLastname; }
+            set { Set(ref myLastname, value); }
         }
     }
 }
